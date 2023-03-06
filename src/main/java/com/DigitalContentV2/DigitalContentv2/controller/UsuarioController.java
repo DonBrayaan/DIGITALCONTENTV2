76bb1cb322bdfe7e6754b3ceba77620015b712e8
BayronRamirez28 <115648457+BayronRamirez28@users.noreply.github.com>
2023-03-06 13:13:23 -0500

    Update UsuarioController.java

--- src/main/java/com/DigitalContentV2/DigitalContentv2/controller/UsuarioController.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/controller/UsuarioController.java
@@ -1,11 +1,17 @@
 package com.DigitalContentV2.DigitalContentv2.controller;
 
+mport java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.repository.query.Param;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +25,10 @@ import com.DigitalContentV2.DigitalContentv2.facadeImp.Barriodao;
 import com.DigitalContentV2.DigitalContentv2.facadeImp.Usuariodao;
 import com.DigitalContentV2.DigitalContentv2.modelo.Barrio;
 import com.DigitalContentV2.DigitalContentv2.modelo.Usuario;
+import com.DigitalContentV2.DigitalContentv2.modelo.UsuarioNotFoundException;
+import com.DigitalContentV2.DigitalContentv2.utility.Utility;
+
+import net.bytebuddy.utility.RandomString;
 
 @Controller
 @RequestMapping("/registro")
@@ -75,6 +85,81 @@ public class UsuarioController {
 	@GetMapping("/forgotPassword")
 	public String  vistaForgot() {
 		return "Ausuario/ForgotPassword";
+	}
+	
+	@PostMapping("/forgotPassword")
+	public String processForgotPassword(HttpServletRequest request, Model model) {
+		String email = request.getParameter("email");
+		String token = RandomString.make(30);
+
+		try {
+			usuarioDao.updateResetPasswordToken(token, email);
+			String resetPasswordLink = Utility.getSiteURL(request) + "/registro/reset_password?token=" + token;
+			sendEmail(email, resetPasswordLink);
+			model.addAttribute("message",
+					"Hemos enviado un enlace para restablecer la contraseña a su correo electrónico. Por favor, compruebe");
+
+		} catch (UsuarioNotFoundException ex) {
+			model.addAttribute("error", ex.getMessage());
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			model.addAttribute("error", "Error al enviar correo electrónico");
+		}
+
+		return "Ausuario/ForgotPassword";
+	}
+
+	public void sendEmail(String recipientEmail, String link) throws MessagingException, UnsupportedEncodingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		helper.setFrom("digitialcontent@gmail.com", "Soporte Digital Content");
+		helper.setTo(recipientEmail);
+
+		String subject = "Aquí está el enlace para restablecer su contraseña";
+
+		String content = "<p>Hola,</p>" + "<p>Ha solicitado restablecer su contraseña.</p>" + "<p>\n"
+				+ "Haga clic en el siguiente enlace para cambiar su contraseña:</p>" + "<p><a href=\"" + link
+				+ "\">Cambiar mi contraseña</a></p>" + "<br>"
+				+ "<p>Ignora este correo electrónico si recuerdas tu contraseña, " + "o no has hecho la solicitud.</p>";
+
+		helper.setSubject(subject);
+
+		helper.setText(content, true);
+
+		mailSender.send(message);
+	}
+
+	@GetMapping("/reset_password")
+	public String showResetPasswordForm(@Param(value = "token") String token, Model model) {
+		Usuario usuario = usuarioDao.getByResetPasswordToken(token);
+		model.addAttribute("token", token);
+
+		if (usuario == null) {
+			model.addAttribute("message", "Invalid Token");
+			return "message";
+		}
+
+		return "Ausuario/ResetPassword";
+	}
+
+	@PostMapping("/reset_password")
+	public String processResetPassword(HttpServletRequest request, Model model) {
+		String token = request.getParameter("token");
+		String password = request.getParameter("password");
+
+		Usuario usuario = usuarioDao.getByResetPasswordToken(token);
+		model.addAttribute("title", "Restablecer su contraseña");
+
+		if (usuario == null) {
+			model.addAttribute("message", "Token invalido");
+			return "Ausuario/message";
+		} else {
+			usuarioDao.updatePassword(usuario, password);
+
+			model.addAttribute("message", "Has cambiado satisfactoriamente tu contraseña.");
+		}
+
+		return "Ausuario/message";
 	}
 }
 
