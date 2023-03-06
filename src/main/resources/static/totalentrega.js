e6fde892cea7ef34189ffcacbcd0db3222ebfdb6
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-03-06 18:16:31 -0500

    AgregarTotalEntregas

--- src/main/resources/static/totalentrega.js
+++ src/main/resources/static/totalentrega.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-	
-	var cantidad = document.getElementById("cant").value;
-	var precio = document.getElementById("precio").value;
 
-	total = precio * cantidad;
-	document.getElementById("total").value = "$"+total;
-	
+$(document).ready(function() {	
+	numero = document.getElementsByClassName("card").length;
+	for (let step = 0; step <= numero; step++) {
+		cantidad = $(".cant:eq("+step+")").val();
+		precio = $(".precio:eq("+step+")").val();
+
+		total = precio * cantidad;
+		$(".total:eq("+step+")").val("$"+total);
+	}
 });
