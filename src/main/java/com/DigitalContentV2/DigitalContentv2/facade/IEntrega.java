15d71bcccecf32a9b3e7efe8799b86ba427937ae
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-03-06 18:17:58 -0500

    Update IEntrega

--- src/main/java/com/DigitalContentV2/DigitalContentv2/facade/IEntrega.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/facade/IEntrega.java
@@ -3,12 +3,17 @@ package com.DigitalContentV2.DigitalContentv2.facade;
 import java.util.List;
 
 import com.DigitalContentV2.DigitalContentv2.modelo.Entrega;
+import com.DigitalContentV2.DigitalContentv2.modelo.Usuario;
 
 public interface IEntrega {
 
 	public List<Entrega> encontrarTodo();
 	public void crear(Entrega entrega, Integer idUsuario);
-	public void actualizar(Entrega entrega);
+	public void actualizar(Entrega entrega, Usuario usuario);
 	public void eliminar(Entrega entrega);
+	public Entrega encontrarId(Integer idEntrega);
+	public List<Entrega> estadoEntregado();
+	public List<Entrega> estadoEnCamino();
+	public List<Entrega> estadoPendiente();
 	
 }
