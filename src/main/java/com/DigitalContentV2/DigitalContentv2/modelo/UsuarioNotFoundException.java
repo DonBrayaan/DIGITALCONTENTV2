5a56198c0dfd285f0d67ee771dad829db1833e38
BayronRamirez28 <115648457+BayronRamirez28@users.noreply.github.com>
2023-03-06 13:03:22 -0500

    Add files via upload

---
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/modelo/UsuarioNotFoundException.java
@@ -0,0 +1,11 @@
+package com.DigitalContentV2.DigitalContentv2.modelo;
+
+public class UsuarioNotFoundException extends Exception {
+
+	public UsuarioNotFoundException(String message) {
+		super(message);
+	}
+
+	
+	
+}
