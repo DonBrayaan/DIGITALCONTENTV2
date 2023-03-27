7f6ae7fced70197bcb5dd2b63088f2e69c5629e8
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-03-27 17:15:42 -0500

    Add files via upload

--- src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/Entregadao.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/Entregadao.java
@@ -7,11 +7,15 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.DigitalContentV2.DigitalContentv2.enums.estadosEntrega;
 import com.DigitalContentV2.DigitalContentv2.facade.IEntrega;
 import com.DigitalContentV2.DigitalContentv2.modelo.Entrega;
 import com.DigitalContentV2.DigitalContentv2.modelo.Usuario;
+import com.DigitalContentV2.DigitalContentv2.modelo.Venta;
 import com.DigitalContentV2.DigitalContentv2.repository.CarRepository;
 import com.DigitalContentV2.DigitalContentv2.repository.EntregaRepository;
+import com.DigitalContentV2.DigitalContentv2.repository.InventarioRepository;
+import com.DigitalContentV2.DigitalContentv2.repository.VentaRepository;
 
 @Transactional
 @Service
@@ -21,7 +25,13 @@ public class Entregadao implements IEntrega{
 	private CarRepository carRepository;
 	
 	@Autowired
+	private VentaRepository ventaRepository;
+	
+	@Autowired
 	private EntregaRepository entregaRepository;
+	
+	@Autowired
+	private InventarioRepository inventarioRepository;
 	
 	public List<Entrega> listEntregasDom(Usuario cliente) {
 		return entregaRepository.findByDom(cliente);
@@ -59,11 +69,29 @@ public class Entregadao implements IEntrega{
  
 	@Override
 	public void actualizar(Entrega entrega, Usuario usuario) {
-		Entrega entrega2 = new Entrega(entrega.getIdEntrega(), 
-				entrega.getFecha(), entrega.getEstado(), usuario);
-		this.entregaRepository.save(entrega2);
+		
+		try {
+			if(entrega.getEstado().equals(estadosEntrega.Entregado)) {
+				Venta venta = new Venta(1, entrega.getFecha());
+				this.ventaRepository.save(venta);
+				
+				Entrega entrega2 = new Entrega();
+				entrega2.setIdEntrega(entrega.getIdEntrega());
+				entrega2.setFecha(entrega.getFecha());
+				entrega2.setEstado(entrega.getEstado());
+				entrega2.setIdCliente(usuario);
+				entrega2.setIdVenta(venta);			
+				this.inventarioRepository.descuentoI(entrega.getIdEntrega());
+				this.entregaRepository.save(entrega2);
+			} else {
+			Entrega entrega2 = new Entrega(entrega.getIdEntrega(), 
+					entrega.getFecha(), entrega.getEstado(), usuario);
+			this.entregaRepository.save(entrega2);
+			}
+		} catch (Exception e) {
+			System.out.println("Algo ha ocurrido durante el preoceso de desucento"+e);
+		}
 	}
-
 	@Override
 	public void eliminar(Entrega entrega) {
 		// TODO Auto-generated method stub
