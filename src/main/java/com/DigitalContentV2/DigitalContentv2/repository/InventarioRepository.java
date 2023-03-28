2af33c6e85e566a710175297c98c0d813a3db9d8
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-03-27 20:56:33 -0500

    Update InventarioRepository.java

--- src/main/java/com/DigitalContentV2/DigitalContentv2/repository/InventarioRepository.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/repository/InventarioRepository.java
@@ -13,7 +13,7 @@ import com.DigitalContentV2.DigitalContentv2.modelo.Inventario;
 public interface InventarioRepository extends JpaRepository<Inventario, Integer>{
 
 	@Query(
-			value = "SELECT inventario.id_inventario,sum(inventario.stock) as stock,count(inventario.id_compra_fk) as id_Compra_fk, inventario.id_producto_fk ,inventario.id_venta_fk FROM inventario group by inventario.id_producto_fk;",
+			value = "SELECT inventario.id_inventario,sum(inventario.stock) as stock,count(inventario.id_compra_fk) as id_Compra_fk, inventario.id_producto_fk ,inventario.id_venta_fk FROM inventario group by inventario.id_producto_fk",
 			nativeQuery = true
 	)
 	List<Inventario> listarContado();
