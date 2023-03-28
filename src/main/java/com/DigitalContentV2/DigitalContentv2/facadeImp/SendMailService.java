a0f6aadceb813b14ebe5e670c2dbae1e228d2917
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-03-27 20:16:32 -0500

    Update SendMailService.java

--- src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/SendMailService.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/SendMailService.java
@@ -48,7 +48,7 @@ public class SendMailService {
 	        MimeMessageHelper helper = new MimeMessageHelper(message,
 	                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
 	                StandardCharsets.UTF_8.name());
-	        helper.addAttachment("template-cover.png", new ClassPathResource("javabydeveloper-email.PNG"));
+	        helper.addAttachment("template-cover.png", new ClassPathResource("javabydeveloper-email.png"));
 	        Context context = new Context();
 	        context.setVariables(mail.getProps());
 	    
